03a61bbf70b2da0d4b4e5b1f9b7c5ce9be41396c
Cognifox Studio <Robertjonsson90@gmail.com>
2026-03-06 16:51:51 +0100

    style: format parent update card

--- lib/presentation/screens/parent_dashboard_screen.dart
+++ lib/presentation/screens/parent_dashboard_screen.dart
@@ -906,8 +906,8 @@ class _UpdateSectionCardState extends State<_UpdateSectionCard> {
       final installedVersion = _installedVersion;
       final updateAvailable = installedVersion != null &&
           _appUpdateService.isUpdateAvailable(
-          installedVersion,
-          release.tagName,
+            installedVersion,
+            release.tagName,
           );
 
       setState(() {
@@ -1053,9 +1053,9 @@ class _UpdateSectionCardState extends State<_UpdateSectionCard> {
     final updateAvailable = hasRelease &&
         hasInstalled &&
         _appUpdateService.isUpdateAvailable(
-        _installedVersion!,
-        release.tagName,
-      );
+          _installedVersion!,
+          release.tagName,
+        );
 
     String statusText;
     if (_errorMessage != null) {
